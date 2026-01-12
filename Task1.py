9c24d1dbbae750b9bba53557110995838f51aff1
EpexAyush <epexayush@gmail.com>
2026-01-13 03:54:36 +0530

    task1 updated

--- Task1.py
+++ Task1.py
@@ -1,6 +1,6 @@
 num1=input("Enter the first number: ")
 num2=input("Enter the second number: ")
-
+print("")
 print("Addition:",int(num1) + int(num2))
 print("Subtraction:",int(num1) - int(num2))
 print("Multiplication:",int(num1)*int(num2))
